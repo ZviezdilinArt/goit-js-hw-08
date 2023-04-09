4bb39eb9c0fadf7639d7834cc0b9665e5eb14667
ZviezdilinArt <118184844+ZviezdilinArt@users.noreply.github.com>
2023-04-09 17:15:04 +0300

    homework_commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,40 @@
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+const markupGallery = createGalleryMarkup(galleryItems);
+
+galleryEl.insertAdjacentHTML('beforeend', markupGallery);
+
+galleryEl.addEventListener('click', preventDefaultLink);
+
+function createGalleryMarkup(galleryArr) {
+  return galleryArr
+    .map(({ preview, original, description }) => {
+      return ` <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          alt="${description}"
+        />
+      </a>
+    </li>`;
+    })
+    .join('');
+}
+
+function preventDefaultLink(evt) {
+    evt.preventDefault();
+}
+
+let gallery = new SimpleLightbox('.gallery a');
+gallery.on('show.simplelightbox', function () {
+  const { defaultOptions } = gallery;
+  defaultOptions.captionsData = 'alt';
+  defaultOptions.captionDelay = 250;
+  defaultOptions.showCounter = false;
+  defaultOptions.scrollZoom = false;
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+const inputEmailEl = document.querySelector('input[name="email"]');
+const textareaMessageEl = document.querySelector('textarea[name="message"]');
+formEl.addEventListener('input', throttle(onFormInput, 500));
+formEl.addEventListener('submit', onFormSubmit);
+
+const FEEDBACK_FORM_STATE = 'feedback-form-state';
+const formData = JSON.parse(localStorage.getItem(FEEDBACK_FORM_STATE)) ?? {};
+restoreFormValue();
+
+function onFormInput(evt) {
+  formData[evt.target.name] = evt.target.value;
+  localStorage.setItem(FEEDBACK_FORM_STATE, JSON.stringify(formData));
+}
+
+function restoreFormValue() {
+  const savedData = JSON.parse(localStorage.getItem(FEEDBACK_FORM_STATE));
+
+  if (savedData) {
+    inputEmailEl.value = savedData[inputEmailEl.name];
+    textareaMessageEl.value = savedData[textareaMessageEl.name];
+  }
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  evt.currentTarget.reset();
+  localStorage.removeItem(FEEDBACK_FORM_STATE);
+  console.log(formData);
+}
